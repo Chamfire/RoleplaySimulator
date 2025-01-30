33bb7a360a704f93082b200f8610f618f89e6fff
TERESA VALERO DÍAZ <tereval2003@gmail.com>
2025-01-30 20:24:27 +0100

    comentado lo de UDP

--- Game.py
+++ Game.py
@@ -92,7 +92,7 @@ class Game:
         self.options = Config(self.width, self.height,None,self.ch1,self.ch2,self.ch3,self.ch4,self.configuration.fps,self.configuration.dmVoice,self.configuration.volMusica, self.configuration.volEffects,self.font)
         self.login = Login(self.width, self.height,None,self.ch1,self.ch2,self.ch3,self.ch4,self.perfil.name,self.perfil.logged,self.perfil.avatarPicPerfil,self.max_length_name,self.font)
         self.seleccionPartidas = SeleccionPartidas(self.width, self.height,None,self.ch1,self.ch2,self.ch3,self.ch4,self.font)
-        self.configuracionPartida = ConfiguracionPartida(self.width, self.height,None,self.ch1,self.ch2,self.ch3,self.ch4,self.local_ip,self.freePort,self.font)
+        self.configuracionPartida = ConfiguracionPartida(self.width, self.height,None,self.ch1,self.ch2,self.ch3,self.ch4,self.local_ip,self.freePortTCP,self.font)
         self.salaEspera = SalaEspera(self.width, self.height,None,self.ch1,self.ch2,self.ch3,self.ch4,self.perfil.avatarPicPerfil,self.perfil.name,self.max_length_name,self.local_ip,self.freePortTCP,self.freePortUDP,self.font,self.perfil.id)
         self.joinPartida = UnionPartida(self.width, self.height,None,self.ch1,self.ch2,self.ch3,self.ch4,self.font,self.perfil.id)
         #Cargamos la música, y precargamos las imágenes y textos en el bufer
@@ -313,7 +313,7 @@ class Game:
         self.s.bind(('', 0)) #encuentra un puerto libre
         free_portTCP = self.s.getsockname()[1] #devuelve el nombre del puerto encontrado
         free_portUDP = None
-        while(free_portUDP == None or free_portUDP == free_portTCP): #que busque un puerto libre distinto al de TCP
-            self.s.bind(('', 0)) #encuentra un puerto libre
-            free_portUDP = self.s.getsockname()[1] #devuelve el nombre del puerto encontrado
+        #while(free_portUDP == None or free_portUDP == free_portTCP): #que busque un puerto libre distinto al de TCP
+        #    self.s.bind(('', 0)) #encuentra un puerto libre
+        #    free_portUDP = self.s.getsockname()[1] #devuelve el nombre del puerto encontrado
         return (free_portTCP,free_portUDP)
