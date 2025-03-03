5bf72a8c4b4e16bc32864831061a42dac258f602
TERESA VALERO DÍAZ <tereval2003@gmail.com>
2025-03-03 10:49:30 +0100

    chat mejorado

--- pruebas2.py
+++ pruebas2.py
@@ -1,17 +1,14 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, LlamaForCausalLM, GPT2LMHeadModel, BitsAndBytesConfig
 import bitsandbytes
-from optimum.bettertransformer import BetterTransformer
-
-torch._dynamo.config.suppress_errors = True  # Evita errores en torch.compile
 
 quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.float16
+    load_in_8bit=True,
+    bnb_8bit_compute_dtype=torch.float16
 )
 
 #modelos descargados: ai-forever/mGPT y NousResearch/Hermes-3-Llama-3.2-3B
-#Hermes 3: Tarda aprox 3 min en generar una respuesta. Problema: es en inglés. 
+#Hermes 3: Tarda aprox 9 min con 750 tokens, temperature=0.8, repetition_penalty=1.1, do_sample=True
 #ai-forever: 9 s es bazofia
 tokenizer = AutoTokenizer.from_pretrained('NousResearch/Hermes-3-Llama-3.2-3B', trust_remote_code=True)
 model = LlamaForCausalLM.from_pretrained(
@@ -22,23 +19,20 @@ model = LlamaForCausalLM.from_pretrained(
     device_map= "cuda"
 )
 
-#device = "cuda" if torch.cuda.is_available() else "cpu"  # Asegurarse de que se use la GPU si está disponible
-#model = model.to(device)
 
-# Texto de entrada
-input_text = "Generate the physical description of an elf in Dnd 5th, who is a barbarian, weights 64 kg and is 36 years old."
+prompts = [
+    """<|im_start|>system
+            You are a dungeon master, of Dnd 5th generation, and you are helping me to create a character.<|im_end|>
+        <|im_start|>user
+            Generate a physical description of an elf, which is barbarian, weights 65kg and is 57 years old.<|im_end|>
+        <|im_start|>assistant""",
+    ]
 
-# Tokenizar el texto
-inputs = tokenizer(input_text, return_tensors="pt")
-inputs = {key: value.to("cuda") for key, value in inputs.items()}
-
-# Generar el texto
-output = model.generate(
-    inputs['input_ids'],
-    attention_mask=inputs['attention_mask'],  # Asegúrate de que el attention_mask esté definido
-    pad_token_id=tokenizer.eos_token_id,  # Establece el pad_token_id si es necesario
-    max_length=256
-)
-
-# Decodificar y mostrar el resultado
-print(tokenizer.decode(output[0], skip_special_tokens=True))
+for chat in prompts:
+    print(chat)
+    encoding = tokenizer(chat, return_tensors="pt")
+    input_ids = encoding.input_ids.to("cuda")
+    attention_mask = encoding.attention_mask.to("cuda")
+    generated_ids = model.generate(input_ids, attention_mask=attention_mask,max_new_tokens=100, temperature=0.8, repetition_penalty=1.1, do_sample=True, eos_token_id=tokenizer.eos_token_id)
+    response = tokenizer.decode(generated_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
+    print(f"Response: {response}")
