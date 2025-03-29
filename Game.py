5f974197c26e22a4d54606f6e66d7c8f40df619a
Chamfire <140488015+Chamfire@users.noreply.github.com>
2025-03-29 11:01:15 +0100

    solved bug against additional parameter due to copy paste

--- Game.py
+++ Game.py
@@ -377,15 +377,15 @@ class Game:
                                 self.partidaScreen.setPersonajeMio(self.joinPartida.getPersonaje())
                                 if(screenToChange == "partida_load_wait"):
                                     self.salaEspera2.setIpANDPortDest(self.joinPartida.getIpANDPortDest())
-                                    self.salaEspera2.initUDPServerAndClient(self.joinPartida.getPortUDPYSocket(),self.joinPartida.getNumJugadoresAndJugadoresAndPort()[2],self.msg_delay,self.ch5,self.max_msgs_udp,self.local_ip)
+                                    self.salaEspera2.initUDPServerAndClient(self.joinPartida.getPortUDPYSocket(),self.joinPartida.getNumJugadoresAndJugadoresAndPort()[2],self.msg_delay,self.max_msgs_udp,self.local_ip)
                                 else:
                                     self.partidaScreen.setIpANDPortDest(self.joinPartida.getIpANDPortDest())
-                                    self.partidaScreen.initUDPServerAndClient(self.joinPartida.getPortUDPYSocket(),self.joinPartida.getNumJugadoresAndJugadoresAndPort()[2],self.msg_delay,self.ch5,self.max_msgs_udp,self.local_ip)
+                                    self.partidaScreen.initUDPServerAndClient(self.joinPartida.getPortUDPYSocket(),self.joinPartida.getNumJugadoresAndJugadoresAndPort()[2],self.msg_delay,self.max_msgs_udp,self.local_ip)
                             elif(self.currentScreen == "seleccionPersonaje"):
                                 self.online = True
                                 self.seleccionPersonaje.setIpANDPortDest(self.joinPartida.getIpANDPortDest())
                                 self.seleccionPersonaje2.setIpANDPort(self.joinPartida.getIpANDPortDest())
-                                self.seleccionPersonaje.initUDPServerAndClient(self.joinPartida.getPortUDPYSocket(),self.joinPartida.getNumJugadoresAndJugadoresAndPort()[2],self.msg_delay,self.ch5,self.max_msgs_udp,self.local_ip)
+                                self.seleccionPersonaje.initUDPServerAndClient(self.joinPartida.getPortUDPYSocket(),self.joinPartida.getNumJugadoresAndJugadoresAndPort()[2],self.msg_delay,self.max_msgs_udp,self.local_ip)
 
                                 # if(not self.online):
                                 #     self.seleccionPersonaje.setCurrentPartida(self.salaEspera.getCurrentPartida())
