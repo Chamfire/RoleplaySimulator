69d9c4a5f470ca0832cf520c1decafca680faeee
Chamfire <140488015+Chamfire@users.noreply.github.com>
2025-04-06 11:34:11 +0200

    Arregladas descripciones de los monstruos

--- Scripts de generacion/DescriptionsMonsters.py
+++ Scripts de generacion/DescriptionsMonsters.py
@@ -21,7 +21,7 @@ model_path = hf_hub_download(model_name, filename=model_file)
 prompts = []
 descripciones = {}
 monstruos = {"no-muerto", "slime", "beholder","troll", "droide","fantasma","objeto animado","cyborg", "lobo wargo","vampiro","oso","hombre lobo","serpiente","cocodrilo","momia","esfinge","goblin","cultista","gnoll","sirena","tiburón","hada","elemental de fuego","elemental de aire","elemental de tierra","elemental de agua","elemental de caos","elemental de planta","kraken","dragón","sombra","fénix","ankheg","basilisco","murciélago","rata","felino salvaje"}
-#2 descripciones por cada tipo de monstruo: 
+#2 descripciones por cada tipo de monstruo, y luego me quedo con la mejor de las 2 descripciones. 
 for monstruo in monstruos:
     for i in range(0,2):
         prompts+= [("""{Eres un dungeon master de Dnd 5e y tienes que generar descripciones para monstruos.}<|eot_id|><|start_header_id|>user<|end_header_id|>
