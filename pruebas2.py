401ca5951e6997c4586bb686f416d2bd0819df1d
Chamfire <140488015+Chamfire@users.noreply.github.com>
2025-04-19 21:25:45 +0200

    Commit de intentos fallidos :(

--- pruebas2.py
+++ pruebas2.py
@@ -1,4 +1,5 @@
 import torch
+from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForCausalLM, LlamaForCausalLM, LlamaTokenizer, BitsAndBytesConfig
 import bitsandbytes
 
@@ -29,31 +30,53 @@ quantization_config = BitsAndBytesConfig(
 
 
 
-tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-3.1-8B-Instruct', trust_remote_code=True)
-#tokenizer = LlamaTokenizer.from_pretrained('', trust_remote_code=True)
-model = LlamaForCausalLM.from_pretrained(
-    "NousResearch/Hermes-3-Llama-3.2-3B",
+# tokenizer = AutoTokenizer.from_pretrained('NousResearch/Meta-Llama-3.1-8B', trust_remote_code=True)
+# #tokenizer = LlamaTokenizer.from_pretrained('', trust_remote_code=True)
+# model = LlamaForCausalLM.from_pretrained(
+#     "NousResearch/Meta-Llama-3.1-8B",
+#     torch_dtype=torch.float16,
+#     quantization_config = quantization_config,
+#     device_map= "cuda"
+#     )
+
+
+# prompts = [
+#     """Genera un acertijo con solución, y la explicación de la solución"""
+#     ]
+
+# for chat in prompts:
+#     print(chat)
+#     encoding = tokenizer(chat, return_tensors="pt")
+#     input_ids = encoding.input_ids.to("cuda")
+#     attention_mask = encoding.attention_mask.to("cuda")
+#     generated_ids = model.generate(input_ids, attention_mask=attention_mask,max_new_tokens=300, pad_token_id=tokenizer.eos_token_id)
+#     response = tokenizer.decode(generated_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
+#     if "." in response:
+#         response = response.rsplit(".", 1)[0] + "."  # Para devolver un párrafo completo
+#     print(f"Response: {response}")
+model_id = 'NousResearch/Meta-Llama-3.1-8B'
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+# Cargar modelo con cuantificación 4-bit o 8-bit
+model = AutoModelForCausalLM.from_pretrained(
+    "NousResearch/Meta-Llama-3.1-8B",
     torch_dtype=torch.float16,
     quantization_config = quantization_config,
     device_map= "cuda"
-    )
+)
 
+generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
-
-prompts = [
-    """{Estás frente a varios amigos, y quieres proponerles un acertijo entretenido, de conocimientos generales, solo con texto}<|eot_id|><|start_header_id|>user<|end_header_id|>
-                        {Usa este formato: <acertijo>\n<solución del acertijo>\n<explicación de la solución>}
+prompt = """{Genera un acertijo que sea simple y fácil de resolver de conocimientos generales}<|eot_id|><|start_header_id|>user<|end_header_id|>
+                        {Sé breve, y responde con el acertijo, su solución, y la explicación de la solución}
                         <|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
-    ]
 
-for chat in prompts:
-    print(chat)
-    encoding = tokenizer(chat, return_tensors="pt")
-    input_ids = encoding.input_ids.to("cuda")
-    attention_mask = encoding.attention_mask.to("cuda")
-    generated_ids = model.generate(input_ids, attention_mask=attention_mask,max_new_tokens=300, pad_token_id=tokenizer.eos_token_id)
-    response = tokenizer.decode(generated_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
-    if "." in response:
-        response = response.rsplit(".", 1)[0] + "."  # Para devolver un párrafo completo
-    print(f"Response: {response}")
-
+output = generator(
+    prompt,
+    max_new_tokens=600,
+    temperature=0.9,
+    top_p=0.95,
+    do_sample=True,
+    repetition_penalty=1.1
+)
+print(output[0]["generated_text"])
