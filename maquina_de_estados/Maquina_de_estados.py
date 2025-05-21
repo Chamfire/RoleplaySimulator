ce653aeb417d8772ee4960f11d9b2afd055f9a07
Chamfire <140488015+Chamfire@users.noreply.github.com>
2025-05-21 14:17:47 +0200

    otro bug de puertas arreglado

--- maquina_de_estados/Maquina_de_estados.py
+++ maquina_de_estados/Maquina_de_estados.py
@@ -981,7 +981,7 @@ class EstadoDeSalaFinal(Estado):
                             #La puerta está cerrada
                             print("puerta cerrada")
                             pygame.mixer.Channel(1).play(self.soundDoor)
-                            text_closed = self.frases_puerta[sala][self.id][0]
+                            text_closed = self.frases_puerta[sala][self.pasilloFromPuerta[1]][0]
                             DM.speak(text_closed) 
                             self.read = False
                             self.GLOBAL.setActionDoor([0,[None,None]]) 
@@ -1355,7 +1355,7 @@ class EstadoDeSalaIntermedia(Estado):
                             #La puerta está cerrada
                             print("puerta cerrada")
                             pygame.mixer.Channel(1).play(self.soundDoor)
-                            text_closed = self.frases_puerta[sala][self.id][0]
+                            text_closed = self.frases_puerta[sala][self.pasilloFromPuerta[1]][0]
                             DM.speak(text_closed) 
                             self.read = False
                             self.GLOBAL.setActionDoor([0,[None,None]]) 
@@ -1792,7 +1792,7 @@ class EstadoDeSalaInicial(Estado):
                             #La puerta está cerrada
                             print("puerta cerrada")
                             pygame.mixer.Channel(1).play(self.soundDoor)
-                            text_closed = self.frases_puerta[sala][self.id][0]
+                            text_closed = self.frases_puerta[sala][self.pasilloFromPuerta[1]][0]
                             DM.speak(text_closed) 
                             self.read = False
                             self.GLOBAL.setActionDoor([0,[None,None]]) 
